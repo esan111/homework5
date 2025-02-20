b59ab8a762591e62bc29ca4f71daf42a381903c1
Emanuel Sanchez <ejsanch07@gmail.com>
2025-02-19 19:44:45 -0600

    link for Card Detail Page

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -8,6 +8,7 @@
     </script>
 
 <template>
+    <RouterLink :to="{name: 'CardDetail', params: {id: username}}">
         <div class="border border-gray-500 rounded-lg shadow bg-gray-400 h-full">
             <img class="object-fill h-480 w-960 rounded-t-lg" v-bind:src="faker.image.urlLoremFlickr({category: 'cats', height: 480, width: 960})" />
             <div class="p-4">
@@ -16,4 +17,5 @@
                 <p class="mb-3 font-normal text-gray-800">{{ bio }}</p>
             </div>
        </div>
+    </RouterLink>
 </template>
